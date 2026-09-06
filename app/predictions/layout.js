ff2de69643605ce85d4b237f38533d5d0cf142a1
frednick <48845021+frednick@users.noreply.github.com>
2026-09-06 19:59:35 +0100

    Add MatchPattern predictions metadata

---
+++ app/predictions/layout.js
@@ -0,0 +1,2 @@
+export const metadata={title:'MatchPattern | Prediction History',description:'Historical football prediction data and results.'};
+export default function PredictionsLayout({children}){return children}
